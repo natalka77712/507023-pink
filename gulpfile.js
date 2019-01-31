30c9c9a5437583f43ed55e91dd0c87a125447652
Евраз <kostousik@mail.ru>
2019-01-31 14:45:49 +0300

    Исправляет недочеты

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
